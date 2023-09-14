3ccc61941a997b98db19cf196bd5f1c54646fdaa
Bernardo Esteves <119340710+berestv@users.noreply.github.com>
2023-09-14 14:15:33 +0100

    Started philosophers

---
+++ philo.h
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bbento-e <bbento-e@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/09/12 12:29:36 by bbento-e          #+#    #+#             */
+/*   Updated: 2023/09/12 14:44:49 by bbento-e         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+#define PHILO_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <limits.h>
+#include <pthread.h>
+#include <sys/time.h>
+
+typedef struct s_philo
+{
+	int	id;
+
+
+
+	
+
+}				t_philo;
+
+#endif //PHILO_H
